f754a2f04a11e1f79ae80cbe88c9aed2deb36c40
ilya <ilvysel3@gmail.com>
2025-01-22 20:40:52 +0300

    Walk was modifed

--- Assets/Scripts/CharacterWalk.cs
+++ Assets/Scripts/CharacterWalk.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterWalk : MonoBehaviour
 {
 
-    //movement speed in units per second
+    //movement speegit d in units per second
     [SerializeField]
     private float movementSpeed = 5f;
-    void Start()
-    {
-        
-    }
 
+    public Rigidbody2D rb;
+    public Animator animator;
+    bool isflip = true;
+
+    Vector2 movement;
     
     void Update()
     {
-        //get the Input from Horizontal axis
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        //update the position
-        if(Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-            transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+        animator.SetFloat("speed", movement.sqrMagnitude);
 
+        if (movement.x < 0f && isflip)
+        {
+            FlipIt();
+        }
+        else if(movement.x > 0f && !isflip)
+        {
+            FlipIt();
+        }
        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime * movementSpeed);
+    }
+
+    void FlipIt()
+    {
+        isflip = !isflip;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
