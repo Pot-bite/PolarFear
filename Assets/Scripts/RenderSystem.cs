a1d80d998d957c5e9dbb330645cd3772d506a1ad
ilya <ilvysel3@gmail.com>
2025-01-23 23:40:41 +0300

    Some news

---
+++ Assets/Scripts/RenderSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RenderSystem : MonoBehaviour
+{
+    private Renderer myrender;
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+    [SerializeField]
+    private bool RunOnlyOnce = false;
+    private float timer;
+    private float timerMax = .1f;
+
+    private void Awake()
+    {
+        myrender = gameObject.GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0f) 
+        {
+            timer = timerMax;
+         myrender.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+         if (RunOnlyOnce)
+         {
+            Destroy(this);
+         }
+        }
+    }
+
+   
+}
